9843911738500334559a55c50146f5813def0e62
FoxIS <andrius.mikonis@gmail.com>
2019-02-12 16:41:06 +0200

    FIX: Minor changes.

--- install/opencv.sh
+++ install/opencv.sh
@@ -1,26 +1,18 @@
 #!/usr/bin/env bash
 
-if [[ "$OSTYPE" == "linux-gnu" || "$OSTYPE" == "linux" ]]; then
-    echo "Using system opencv package"
-    exit
-fi
+git clone https://github.com/opencv/opencv.git opencv3
+git clone https://github.com/opencv/opencv_contrib.git opencv3_contrib
+cd opencv3_contrib
+git checkout d879ea48b696ec9ba8d1d5fd4d9688fa230e8515
+cd ../opencv3
+git checkout 976ee4468276d8f90403cc0885b8fd8576c58f4d
+mkdir build
+cd build
 
-if [ ! -d "$DIRECTORY" ]; then
-    apt-get -y install libopencv-dev build-essential cmake git libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22 libdc1394-22-dev libhdf5-serial-dev libjpeg-dev libpng12-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip
-    git clone https://github.com/opencv/opencv.git opencv3
-    git clone https://github.com/opencv/opencv_contrib.git opencv3_contrib
-    cd opencv3_contrib
-    git checkout d879ea48b696ec9ba8d1d5fd4d9688fa230e8515
-    cd ../opencv3
-    git checkout 976ee4468276d8f90403cc0885b8fd8576c58f4d
-    mkdir build
-fi
-
-if [ "$2" = "python3" ]; then
-    cd $1/opencv3/build
+if [ "$1" = "python3" ]; then
     cmake -DCMAKE_BUILD_TYPE=RELEASE \
-          -DCMAKE_INSTALL_PREFIX=$1/opencv3 \
-          -DOPENCV_EXTRA_MODULES_PATH=$1/opencv3_contrib/modules \
+          -DCMAKE_INSTALL_PREFIX=../../opencv3 \
+          -DOPENCV_EXTRA_MODULES_PATH=../../opencv3_contrib/modules \
           -DWITH_CUDA=ON \
           -DCUDA_GENERATION=Kepler \
           -DWITH_CUBLAS=ON \
@@ -59,10 +51,9 @@ if [ "$2" = "python3" ]; then
           ..
     make -j12 && make install
 else
-    cd $1/opencv3/build
     cmake -DCMAKE_BUILD_TYPE=RELEASE \
-          -DCMAKE_INSTALL_PREFIX=$1/opencv3 \
-          -DOPENCV_EXTRA_MODULES_PATH=$1/opencv3_contrib/modules \
+          -DCMAKE_INSTALL_PREFIX=../../opencv3 \
+          -DOPENCV_EXTRA_MODULES_PATH=../../opencv3_contrib/modules \
           -DWITH_CUDA=ON \
           -DCUDA_GENERATION=Kepler \
           -DWITH_CUBLAS=ON \
