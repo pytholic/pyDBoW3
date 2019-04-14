3bc2ea991ca3e40b84baa067305a49a6af6c2490
FoxIS <andrius.mikonis@gmail.com>
2019-04-14 22:39:22 +0000

    FIX: Support building on arm.

--- install/opencv.sh
+++ install/opencv.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 CWD=`pwd`
+VERSION=4.0.1
+
 git clone https://github.com/opencv/opencv.git opencv3
 git clone https://github.com/opencv/opencv_contrib.git opencv3_contrib
 cd opencv3_contrib
-git checkout 3.4.2
+git checkout $VERSION
 cd ../opencv3
-git checkout 3.4.2
+git checkout $VERSION
+
 mkdir build
 cd build
 
 if [ -z "$OPENCV_INSTALL_LOCALLY" ]; then
-  OPENCV_PREFIX=""
+  OPENCV_PREFIX="-DCMAKE_INSTALL_PREFIX=/usr/local"
 else
   OPENCV_PREFIX="-DCMAKE_INSTALL_PREFIX=$CWD/opencv3"
 fi
@@ -27,22 +30,22 @@ if [ "$BUILD_PYTHON" = "python3" ]; then
            $OPENCV_PREFIX \
           -DOPENCV_EXTRA_MODULES_PATH=$CWD/opencv3_contrib/modules \
           -DWITH_CUDA=OFF \
-          -DCUDA_GENERATION=Kepler \
-          -DWITH_CUBLAS=ON \
           -DCUDA_FAST_MATH=ON \
-          -DWITH_NVCUVID=ON \
+	  -DOPENCV_ENABLE_NONFREE=ON \
           -DWITH_OPENGL=OFF \
-          -DWITH_OPENCL=OFF \
-          -DENABLE_AVX=ON \
+          -DWITH_OPENCL=ON \
+	  -DBUILD_WITH_DEBUG_INFO=OFF \
           -DENABLE_FAST_MATH=ON \
           -DBUILD_TIFF=ON \
           -DWITH_CSTRIPES=ON \
-          -DWITH_EIGEN=OFF \
+          -DWITH_EIGEN=ON \
+	  -DWITH_QT=OFF \
+          -DWITH_GTK=OFF \
           -DWITH_IPP=ON \
-          -DWITH_TBB=ON \
+          -DWITH_TBB=OFF \
           -DWITH_OPENMP=ON \
-          -DENABLE_SSE42=ON \
           -DWITH_V4L=ON \
+          -DWITH_GSTREAMER=OFF \
           -DWITH_VTK=OFF \
           -DBUILD_opencv_java=OFF \
           -DBUILD_EXAMPLES=OFF \
@@ -63,7 +66,7 @@ if [ "$BUILD_PYTHON" = "python3" ]; then
           -DPYTHON3_INCLUDE_DIR=$PYTHON_INCLUDES \
           -DPYTHON3_PACKAGES_PATH=$PYTHON_LIBS \
           ..
-    make -j12 || exit 1
+    make -j4 || exit 1
     sudo make install
 else
     echo "Building OpenCV with Python2 support"
